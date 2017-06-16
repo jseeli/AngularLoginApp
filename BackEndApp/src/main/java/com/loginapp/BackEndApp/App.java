3edb8c9109d41c34d7f2f8d1e363db30d651c7a4
SRINIVAS PATNAIK <srinssit@gmail.com>
2017-06-16 23:45:50 +0530

    BackEnd App- Model , DAO and Test Cases

---
+++ BackEndApp/src/main/java/com/loginapp/BackEndApp/App.java
@@ -0,0 +1,13 @@
+package com.loginapp.BackEndApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
